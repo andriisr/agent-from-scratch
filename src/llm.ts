5814c00f685845279d48969e7fd3c35cff115b2d
Andrii Rybakov <wpandrewbox@gmail.com>
2025-01-15 20:01:20 -0500

    end of step 2

--- src/llm.ts
+++ src/llm.ts
@@ -1,18 +1,16 @@
+import type { AIMessage } from '../types';
 import { openai } from './ai';
 
 export const runLLM = async ({
-    userMessage
-}: {userMessage: string}) => {
+    messages
+}: {
+    messages: AIMessage[]
+}) => {
     const response = await openai.chat.completions.create({
         model: 'gpt-4o-mini',
         temperature: 0.1,
-        messages: [
-            {
-                role: 'user',
-                content: userMessage
-            }
-        ]
+        messages
     })
-    
+
     return response.choices[0].message.content;
 }
